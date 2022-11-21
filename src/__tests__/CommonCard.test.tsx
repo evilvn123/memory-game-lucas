dd08ddfc17d4562ccd1a9491831c485939dff875
Viet.h <viet.h@hivery.com>
2022-11-21 23:55:31 +0700

    update snap

--- src/__tests__/CommonCard.test.tsx
+++ src/__tests__/CommonCard.test.tsx
@@ -1,5 +1,6 @@
 import { cleanup, render, screen } from "@testing-library/react";
 import { CommonCard, Back, Front } from "../components/CommonCard";
+import renderer from "react-test-renderer";
 
 afterEach(cleanup);
 
@@ -43,3 +44,54 @@ it("Render component with customStyle props", () => {
     background: "rgba(0, 128, 0, 0.3)",
   });
 });
+
+it("renders correctly 1", () => {
+  const tree = renderer.create(<CommonCard open={false} />).toJSON();
+  expect(tree).toMatchInlineSnapshot(`
+<div
+  className="common-card "
+  data-testid="common-card"
+>
+  <div
+    className="inner "
+    data-testid="inner"
+  >
+    <div
+      className="common-card-front"
+      data-testid="common-card-front"
+    />
+    <div
+      className="common-card-back"
+      data-testid="common-card-back"
+    />
+  </div>
+</div>
+`);
+  expect(tree).toMatchSnapshot();
+});
+
+it("renders correctly 2", () => {
+  const tree = renderer.create(<CommonCard open={true} />).toJSON();
+  expect(tree).toMatchInlineSnapshot(`
+<div
+  className="common-card "
+  data-testid="common-card"
+>
+  <div
+    className="inner open"
+    data-testid="inner"
+  >
+    <div
+      className="common-card-front"
+      data-testid="common-card-front"
+    />
+    <div
+      className="common-card-back"
+      data-testid="common-card-back"
+    />
+  </div>
+</div>
+`);
+  expect(tree).toMatchSnapshot();
+});
+
